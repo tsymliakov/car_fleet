dc65c6de4258cfc3c5435431a5250155b6ea65da
Denis <makrotheeast@gmail.com>
2023-05-26 01:26:12 +0300

    Added brand model

--- car_fleet/vehicle/admin.py
+++ car_fleet/vehicle/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Vehicle
+from .models import Vehicle, Brand
 
 
 @admin.register(Vehicle)
 class AdminVehicle(admin.ModelAdmin):
-    list_display = ['id', 'total_cost']
+    list_display = ['id', 'total_cost', 'brand_name']
+
+    def brand_name(self, obj):
+        return obj.brand.name
+
+
+@admin.register(Brand)
+class AdminBrand(admin.ModelAdmin):
+    list_display = ['id', 'name', 'body_type', 'tank_capacity']
--- car_fleet/vehicle/models.py
+++ car_fleet/vehicle/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.TextField()
+    class BodyType(models.TextChoices):
+        CARGO = "CARG", "Cargo"
+        BUS = "BUS", "Bus"
+        PICKUP = "PICK", "Pickup"
+        SEDAN = "SEDA", "Sedan"
+        CABRIOLET = "CABR", "Cabriolet"
+
+    body_type = models.CharField(
+        max_length=4,
+        choices=BodyType.choices,
+        default=BodyType.SEDAN
+    )
+    tank_capacity = models.IntegerField()
+    passanger_seat_count = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Vehicle(models.Model):
     rental_per_hour = models.FloatField()
     total_cost = models.IntegerField()
     mileage = models.IntegerField()
     production_year = models.DateField()
+
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
