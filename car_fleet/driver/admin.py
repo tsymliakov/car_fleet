a8ce52009a515702ef40423b98aa9d92a2bd10ce
Denis <makrotheeast@gmail.com>
2023-05-31 08:58:20 +0300

    Limited the ability to change field of driver in admin panel

--- car_fleet/driver/admin.py
+++ car_fleet/driver/admin.py
@@ -7,11 +7,13 @@ from vehicle.models import Vehicle
 class AdminDriver(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'salary', 'enterprise', 'vehicles', 'active_vehicle']
 
-    # TODO: сделать отображение компании, в которой работает водитель
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["active_vehicle"].queryset = Vehicle.objects.filter(active_driver__isnull=True).filter(driver=obj)
+        return form
+
     def enterprise(self, obj):
         return obj.enterprise
 
-    # TODO: сделать отображение id всех автомобилей, к которым прикреплен
-    # водитель
     def vehicles(self, obj):
         return tuple(Vehicle.objects.filter(driver=obj))
