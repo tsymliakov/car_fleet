26c3ec6a5cbf28daac343f3f7636d4ee6aff7b0c
Denis <makrotheeast@gmail.com>
2023-05-31 09:41:16 +0300

    Fixed bug in admin panel

--- car_fleet/driver/admin.py
+++ car_fleet/driver/admin.py
@@ -10,6 +10,7 @@ class AdminDriver(admin.ModelAdmin):
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
         form.base_fields["active_vehicle"].queryset = Vehicle.objects.filter(active_driver__isnull=True).filter(driver=obj)
+        form.base_fields["vehicle"].queryset = Vehicle.objects.filter(enterprise=obj.enterprise)
         return form
 
     def enterprise(self, obj):
