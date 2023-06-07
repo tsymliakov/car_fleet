d62a3d3ac95b4f45862520c73ca1f17a604ebfe2
Denis <makrotheeast@gmail.com>
2023-06-07 08:47:50 +0300

    Some more work has been done

--- car_fleet/enterprise/models.py
+++ car_fleet/enterprise/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from faker import Faker
 
 
 class Enterprise(models.Model):
@@ -7,3 +8,10 @@ class Enterprise(models.Model):
 
     def __str__(self):
         return f'id: {self.id}, {self.name}'
+
+
+def generate_data_for_enterprise():
+    fake = Faker()
+    name  = fake.text().split()[-2].capitalize()
+    location = fake.city()
+    return name, location
