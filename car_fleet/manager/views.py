e07a9f158abebd0b0de9735c368095fb655be859
Denis <makrotheeast@gmail.com>
2023-06-24 22:39:05 +0300

    Added manager auth form

--- car_fleet/manager/views.py
+++ car_fleet/manager/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponseForbidden
+from django.views import View
 from django.shortcuts import render
+from enterprise.models import Enterprise
+from manager.models import Manager
 
-# Create your views here.
+
+class Companies(View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        if isinstance(getattr(user, 'manager', None), Manager):
+            user = request.user
+            companies = Enterprise.objects.filter(manager=user)
+            return render(request,
+                          'manager/index_enterprise.html',
+                          context={'companies' : list(companies),
+                                   'manager' : user})
+
+        return HttpResponseForbidden('Необходима аутентификация в качестве менеджера\n')
