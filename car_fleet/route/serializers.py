faa91956bfdc90f3b5ab995b3825833c2f11e2de
Denis <makrotheeast@gmail.com>
2023-07-14 10:42:07 +0300

    Have ended working with API

--- car_fleet/route/serializers.py
+++ car_fleet/route/serializers.py
@@ -1,26 +1,45 @@
 import pytz
 from rest_framework import serializers
 from .models import Route
+from point.models import Point
+from geopy.geocoders import Nominatim
 
 
-def get_geoinfo(altitude, logitude):
-    """
-    Функция обращается к стороннему API, чтобы получить адрес точки.
-    """
+def get_location(point):
+    latitude = point.point.y
+    longitude = point.point.x
+
+    geolocator = Nominatim(user_agent="pls_dont_ban_my_home_proj")
+    location = geolocator.reverse((latitude,longitude))
+
+    if location is not None:
+        return location
+
+    return (latitude, longitude)
 
 
 class RouteSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         timezone = pytz.timezone(instance.vehicle.enterprise.timezone)
-        self.fields['start'] = serializers.DateTimeField(default_timezone=timezone)
-        self.fields['end'] = serializers.DateTimeField(default_timezone=timezone)
+        # self.fields['start'] = serializers.DateTimeField(default_timezone=timezone)
+        # self.fields['end'] = serializers.DateTimeField(default_timezone=timezone)
         representation = super().to_representation(instance)
 
-        self.fields['start_point'] = serializers.CharField()
-        representation['start_point'] = 'Start'
+        self.fields['route_start_location'] = serializers.CharField()
 
-        self.fields['end_point'] = serializers.CharField()
-        representation['end_point'] = 'End'
+        self.fields['route_end_location'] = serializers.CharField()
+
+        start_point = Point.objects.filter(vehicle__id=representation['vehicle'])\
+                                   .filter(time__gte=representation['start']).order_by('time')[0]
+
+        end_point = Point.objects.filter(vehicle__id=representation['vehicle'])\
+                                   .filter(time__lte=representation['end'])[0]
+
+        start_location = get_location(start_point)
+        end_location = get_location(end_point)
+
+        representation['route_start_location'] = start_location.__str__()
+        representation['route_end_location'] = end_location.__str__()
 
         return representation
 
--- car_fleet/route/views.py
+++ car_fleet/route/views.py
@@ -14,15 +14,12 @@ class RoutesIndex(APIView):
         end_datetime = self.request.GET.get('end_time', None)
 
         if start_datetime is None or end_datetime is None:
-            routes = Route.objects.all()
+            routes = Route.objects.filter(vehicle__id=vehicle_id)
         else:
             routes = Route.objects\
                 .filter(start__gte=start_datetime)\
                 .filter(end__lte=end_datetime)\
                 .filter(vehicle__id=vehicle_id)
-
-        # Далее требуется передать это в сериализатор, который добавит лополнительные поля. Такие как
-        # адрес начальной и конечной точки.
 
         serializer = RouteSerializer(routes, many=True)
 
