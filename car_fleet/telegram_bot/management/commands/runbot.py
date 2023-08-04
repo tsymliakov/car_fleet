0e19f53e2bfcaebad4e7d39682d359471ac23a22
Denis <makrotheeast@gmail.com>
2023-08-05 01:58:12 +0300

    Added functionality to telegram bot

--- car_fleet/telegram_bot/management/commands/runbot.py
+++ car_fleet/telegram_bot/management/commands/runbot.py
@@ -1,18 +1,42 @@
+from datetime import datetime
+from json import load
+
 from django.core.management.base import BaseCommand
 from django.contrib.auth import authenticate, login
 from telebot import types
 import telebot
 
+from enterprise.models import Enterprise
+from vehicle.models import Vehicle
 
-token = '5642759020:AAGYvoH8emqBdbJYLGlJUo7jKFRZjsQI-aM'
+with open("settings.json", "r") as settings_file:
+    LOCAL_SETTINGS = load(settings_file)
+
+try:
+    token = LOCAL_SETTINGS['telegram_token']
+except:
+    raise Exception("Telegram bot token hasn't been provided!")
+
 user_credentials = {}
 bot = telebot.TeleBot(token)
 
 
+@bot.message_handler(commands=['debug'])
+def show_debug(message):
+    info = [
+        str(user_credentials),
+        message.chat.id
+    ]
+    bot.send_message(message.chat.id, str(info))
+
+
 @bot.message_handler(commands=['start'])
 def menu(message):
+    print(user_credentials)
     if user_credentials.get(message.chat.id):
         bot.send_message(message.chat.id, 'Вы уже аутентифицированы.')
+        bot.register_next_step_handler(message, manager_menu)
+        return
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     login_button = types.KeyboardButton("Войти в систему")
@@ -20,17 +44,124 @@ def menu(message):
     bot.send_message(message.chat.id, 'Приветствую. Войдете в систему?', reply_markup=markup)
 
 
+def manager_menu(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton("Получить пробег по конкретной машине")
+    button2 = types.KeyboardButton("Получить пробег машин предприятия")
+    markup.add(button1, button2)
+    bot.send_message(message.chat.id, 'Выберете жалаемое действие.', reply_markup=markup)
+
+
+def get_car_mileage(message):
+    if user_credentials.get(message.chat.id) is None:
+        bot.send_message(message.chat.id, 'Вы не аутентифицированы.')
+        menu(message)
+        return
+
+    format = "%d-%m-%Y"
+
+    try:
+        info = message.text.split()
+        vehicle_id = int(info[0])
+
+        try:
+            vehicle = Vehicle.objects.get(id=vehicle_id)
+        except:
+            bot.send_message(message.chat.id, 'Автомобиля с таким ID нет.')
+            manager_menu(message)
+            return
+
+        if not vehicle.company.manager.filter(id=user_credentials[message.chat.id].id).exists():
+            bot.send_message(message.chat.id, 'У Вас нет доступа этой компании, владеющей этим автомобилем.')
+            manager_menu(message)
+            return
+
+        start_date = datetime.strptime(info[1], format)
+        end_date = datetime.strptime(info[2], format)
+
+        # подсчет пробега у целого предприятия не реализован
+    except:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button = types.KeyboardButton("Вернуться в меню менеджера")
+        markup.add(button)
+        bot.send_message(message.chat.id, 'Вы не соблюли формат.',
+                         reply_markup=markup)
+        return
+
+
+def get_company_mileage(message):
+    if user_credentials.get(message.chat.id) is None:
+        bot.send_message(message.chat.id, 'Вы не аутентифицированы.')
+        menu(message)
+        return
+
+    format = "%d-%m-%Y"
+
+    try:
+        info = message.text.split()
+        company_id = int(info[0])
+
+        try:
+            company = Enterprise.objects.get(id=company_id)
+        except:
+            bot.send_message(message.chat.id, 'Компании с таким ID нет.')
+            manager_menu(message)
+            return
+
+        if not company.manager.filter(id=user_credentials[message.chat.id].id).exists():
+            bot.send_message(message.chat.id, 'У Вас нет доступа к этой компании.')
+            manager_menu(message)
+            return
+
+        start_date = datetime.strptime(info[1], format)
+        end_date = datetime.strptime(info[2], format)
+
+        # подсчет пробега у целого предприятия не реализован
+    except:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button = types.KeyboardButton("Вернуться в меню менеджера")
+        markup.add(button)
+        bot.send_message(message.chat.id, 'Вы не соблюли формат.',
+                         reply_markup=markup)
+        return
+
+
 @bot.message_handler(content_types=['text'])
 def message_reply(message):
     if message.text == "Войти в систему":
-        markup = telebot.types.ReplyKeyboardRemove()
+        bot.register_next_step_handler(message, handle_credentials)
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button = types.KeyboardButton("Вернуться")
+        markup.add(button)
+
         bot.send_message(message.chat.id,
                          "Введите никнейм и пароль в формате 'username password'",
                          reply_markup=markup)
-        bot.register_next_step_handler(message, handle_credentials)
+        return
+
+    if message.text == "Получить пробег по конкретной машине":
+        bot.send_message(message.chat.id,
+                         "Укажите ID машины и диапазон дат в формате \"ID DD-MM-YYYY DD-MM-YYYY\"")
+        bot.register_next_step_handler(message, get_car_mileage)
+        return
+
+    if message.text == "Получить пробег машин предприятия":
+        bot.send_message(message.chat.id,
+                         "Укажите ID компании и диапазон дат в формате \"ID DD-MM-YYYY DD-MM-YYYY\"")
+        bot.register_next_step_handler(message, get_company_mileage)
+        return
+
+    if message.text == "Вернуться в меню менеджера":
+        bot.register_next_step_handler(message, manager_menu)
+        return
 
 
 def handle_credentials(message):
+    if message.text == "Вернуться":
+        menu(message)
+        return
+
     nick_pass = message.text
 
     try:
@@ -46,17 +177,22 @@ def handle_credentials(message):
 
     if nickname == 'Котичка' and password == "123":
         bot.send_message(message.chat.id, "Котик тебя поцеловал, котичка милая :-*")
-
+        bot.send_message(message.chat.id, "Но все же логин или пароль не верны." +
+                         " Попробуйте еще.")
+        bot.register_next_step_handler(message, handle_credentials)
+        return
 
     user = authenticate(username=nickname, password=password)
 
+    if not user:
+        bot.send_message(message.chat.id, "Не верный логин или пароль. Попробуйте еще раз.")
+        bot.register_next_step_handler(message, handle_credentials)
+        return
+
     user_credentials[message.chat.id] = user
 
-    print(nickname, password)
+    manager_menu(message)
 
-    bot.send_message(message.chat.id, "На этом все. Пока!")
-
-    menu(message)
 
 class Command(BaseCommand):
     help = "Starts telegram bot"
